4ec0c058e812ea6b95af9af143dc1944ad7a5ed2
farouk khaled <elghriwfarouk@gmail.com>
2024-02-28 09:24:43 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyTrain01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUS()
+        {
+            return View();
+        }
+        public IActionResult ContactUS() 
+        {
+            return View();
+        }
+
+       
+    }
+}
